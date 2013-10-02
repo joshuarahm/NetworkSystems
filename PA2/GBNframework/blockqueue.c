eac64f6ef1461fd1f9d05a9a9890325a8f087e51
Zach Anders <Zacharia.Anders@gmail.com>
2013-10-01 20:39:33 -0600

    Added block queue

---
+++ PA2/GBNframework/blockqueue.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include "chunkqueue.h"
+
+void queue_init(block_queue_t *queue, uint32_t qsize) {
+	if (queue->_m_data)
+		free(queue->_m_data);
+	queue->_m_size = qsize;
+	queue->_m_head = 0;
+	queue->_m_tail = 0;
+
+	queue->_m_data = malloc(sizeof(data_block_t)*qsize);
+	queue->_m_write_sem = malloc(sizeof(sem_t));
+	queue->_m_read_sem = malloc(sizeof(sem_t));
+	queue->_m_modify_lock = malloc(sizeof(pthread_mutex_t));
+	
+	sem_init(queue->_m_write_sem, 0, queue->_m_size);
+	sem_init(queue->_m_read_sem, 0, 0);
+	pthread_mutex_init(queue->_m_modify_lock, NULL);
+}
+
+void push_chunk(block_queue_t *queue, data_block_t *data) {
+	while (sem_wait(queue->_m_write_sem)); //sem_wait can be interrupted, so we loop until it returns success. (return code 0)
+
+	pthread_mutex_lock(queue->_m_modify_lock);
+
+	sem_post(queue->_m_read_sem);
+	queue->_m_data[queue->_m_head] = data;
+	queue->_m_head++;
+	queue->_m_head %= queue->_m_size;
+
+	pthread_mutex_unlock(queue->_m_modify_lock);
+}
+
+data_block_t* pop_chunk(block_queue_t *queue) {
+	data_block_t *ret;
+	while (sem_wait(queue->_m_read_sem)); //sem_wait can be interrupted, so we loop until it returns success. (return code 0)
+
+	pthread_mutex_lock(queue->_m_modify_lock);
+
+	sem_post(queue->_m_write_sem);
+	ret = queue->_m_data[queue->_m_tail];
+	queue->_m_tail++;
+	queue->_m_tail %= queue->_m_size;
+
+	pthread_mutex_unlock(queue->_m_modify_lock);
+	return ret;
+}
+	
+
+void queue_free(block_queue_t *queue) {
+	sem_destroy(queue->_m_read_sem);
+	sem_destroy(queue->_m_write_sem);
+	pthread_mutex_destroy(queue->_m_modify_lock);
+
+	free(queue->_m_data);
+	free(queue->_m_read_sem);
+	free(queue->_m_write_sem);
+	free(queue->_m_modify_lock);
+}
---
+++ PA2/GBNframework/blockqueue.h
@@ -0,0 +1,37 @@
+#ifndef _CHUNK_QUEUE_H
+#define _CHUNK_QUEUE_H
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdint.h>
+
+
+/* Synchronized blocking queue for passing chunks between threads. */
+typedef struct {
+	uint8_t *_m_data;
+	uint32_t _m_len;
+} data_block_t;
+
+typedef struct {
+	data_block_t **_m_data;
+	uint32_t _m_head;
+	uint32_t  _m_tail;
+	uint32_t _m_size;
+	sem_t *_m_write_sem, *_m_read_sem;
+	pthread_mutex_t* _m_modify_lock;
+} block_queue_t;
+
+/* Initialize the block queue. */
+void queue_init(block_queue_t *queue, uint32_t qsize);
+
+/* Push a data block onto the queue. */
+void push_chunk(block_queue_t *queue, data_block_t *data);
+
+/* Remove the top data block from the queue. */
+data_block_t* pop_chunk(block_queue_t *queue);
+
+/* Frees internal queue resources */
+void queue_free(block_queue_t *queue);
+
+#endif
+

e9a5a565748c49a5c2a254fb1fff0357c064effe
Zach Anders <Zacharia.Anders@gmail.com>
2013-10-02 02:42:36 -0600

    Fixed receive issue

--- PA2/GBNframework/gbn_read.c
+++ PA2/GBNframework/gbn_read.c
@@ -58,7 +58,17 @@ int gbn_read_thread_main ( gbn_socket_t *socket) {
 					}
 				}
 
-				while (get_receive_frame(socket, 0)->_m_type != gbn_packet_type_uninitialized) {
+				while (get_receive_frame(socket, 0)._m_type != gbn_packet_type_uninitialized) {
+					gbn_packet_t *cur = &get_receive_frame(socket, 0);
+					block_queue_push_chunk(&(socket->_m_receive_buffer), to_rcv_queue);
+					socket->_m_receive_window._m_recv_counter++;
+					socket->_m_receive_window._m_head++;
+					cur->_m_type = gbn_packet_type_uninitialized;
+				}
+
+				free(packet);
+				to_rcv_queue->_m_data = packet->_m_payload;
+				to_rcv_queue->_m_len  = packet->_m_size;
 				ack_packet = malloc(sizeof(gbn_packet_t));
 				ack_packet->_m_seq_number = packet->_m_seq_number;
 				ack_packet->_m_size = 0;
@@ -68,10 +78,7 @@ int gbn_read_thread_main ( gbn_socket_t *socket) {
 
 				bytes_sent = sendto(socket->_m_sockfd, ack_serial, pack_size, 0, (struct sockaddr *) &(socket->_m_to_addr), sizeof(struct sockaddr_in));
 
-				to_rcv_queue->_m_data = packet->_m_payload;
-				to_rcv_queue->_m_len  = packet->_m_size;
-				free(packet);
-				block_queue_push_chunk(&(socket->_m_receive_buffer), to_rcv_queue);
+
 				break;
 			default:
 				break;

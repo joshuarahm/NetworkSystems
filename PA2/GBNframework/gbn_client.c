338a979c676d7198f73fe5b70f4d1090b229d619
Joshua Rahm <joshua.rahm@colorado.edu>
2013-10-03 22:14:23 -0600

    Added client mockup

---
+++ PA2/GBNframework/gbn_client.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "gbn_socket.h"
+
+#define READ_SIZE (4096<<1)
+
+int main( int argc, char** argv ) {
+    gbn_socket_t* sock;
+
+    if( ! argv[1] ) {
+        fprintf( stderr, "File argument required\n" );
+        return 1;
+    }
+
+    sock = gbn_socket_open_client( "localhost", 5432 ) ;
+
+    FILE* read = fopen( argv[1], "r" );
+    char buffer[READ_SIZE];
+    size_t bytes_read;
+
+    while( (bytes_read = fread(buffer, READ_SIZE, 1, read)) > 0 ) {
+        gbn_socket_write( sock, buffer, bytes_read );
+    }
+    
+    gbn_socket_close( sock );
+    return 0;
+}

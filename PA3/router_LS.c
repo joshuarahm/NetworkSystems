f9a65e1430216544d74f08e2ef3ef586052e8b80
ZachAnders <Zacharia.Anders@gmail.com>
2013-11-12 22:26:58 -0700

    Continued initalization

---
+++ PA3/router_LS.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char* argv[]) {
+	printf("Router stuff goes here.\n");
+	return -1;
+}
